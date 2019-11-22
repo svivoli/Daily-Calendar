6f2c196adecc87f2e77692ee1828932e20173dae
Sarah Vivoli <snvivoli@gmail.com>
2019-11-22 11:57:12 -0800

    cleaned up design

--- script.js
+++ script.js
@@ -13,8 +13,6 @@ var timeDiv = $(".time");
 var note = $(".note");
 var taskForms = $(".task");
 
-calendar.html("<h4>" + date + "<h4>" + calDivs.html());
-
 var input10 = $("#task10");
 var form10 = $("#form10");
 var input11 = $("#task11");
@@ -31,6 +29,8 @@ var input4 = $("#task4");
 var form4 = $("#form4");
 var input5 = $("#task5");
 var form5 = $("#form5");
+
+calendar.html("<h4>" + date + "<h4>" + calDivs.html());
 
 var notes = {
    note10: {
@@ -73,30 +73,14 @@ var notes = {
 
 // prevent form from refreshing page on submit, enter text to field
 
-var form10 = $("#form10");
-var input11 = $("#task11");
-var form11 = $("#form11");
-var input12 = $("#task12");
-var form12 = $("#form12");
-var input1 = $("#task1");
-var form1 = $("#form1");
-var input2 = $("#task2");
-var form2 = $("#form2");
-var input3 = $("#task3");
-var form3 = $("#form3");
-var input4 = $("#task4");
-var form4 = $("#form4");
-var input5 = $("#task5");
-var form5 = $("#form5");
+// event.preventDefault();
+// var newTask = localStorage.getItem("input");
+// input10.push(newTask);
+// localStorage.setItem("input", input);
 
-   // event.preventDefault();
-   // var newTask = localStorage.getItem("input");
-   // input10.push(newTask);
-   // localStorage.setItem("input", input);
-   
 var forms = $("#form");
 
-forms.on("submit", function(event) {
+forms.on("submit", function (event) {
    event.preventDefault();
 });
 
@@ -105,129 +89,160 @@ forms.on("submit", function(event) {
 var add10 = $(".addBttn10");
 var input10 = $("#task10");
 
-add10.on("click", function(event) {
-   event.preventDefault();
-   
-   localStorage.setItem("input", JSON.stringify(notes));
 
-   var newInputs = JSON.parse(localStorage.getItem("input"));
-   input10.text = newInputs.note10.task
-
-   console.log(input10);
-});
 
 
 ////////////////////////////////
 ////// working code below //////
 ///////////////////////////////
 
-
-// change color of task depending on time of day
-
 var today = new Date();
 var hour = today.getHours();
 
-// hour = 14;
+hour = 14;
 
+// set task colors
+
+function setTasks() {
+   if (notes.note10.time < hour) {
+      $('input[name=task10]').attr("style", "background-color: #c7c7c7");
+   } else if (notes.note10.time > hour) {
+      $('input[name=task10]').attr("style", "background-color: #8ceac9");
+   } else {
+      $('input[name=task10]').attr("style", "background-color: #e6476f");
+   }
+   if (notes.note11.time < hour) {
+      $('input[name=task11]').attr("style", "background-color: #c7c7c7");
+   } else if (notes.note11.time > hour) {
+      $('input[name=task11]').attr("style", "background-color: #8ceac9");
+   } else {
+      $('input[name=task11]').attr("style", "background-color: #e6476f");
+   }
+   if (notes.note12.time < hour) {
+      $('input[name=task12]').attr("style", "background-color: #c7c7c7");
+   } else if (notes.note12.time > hour) {
+      $('input[name=task12]').attr("style", "background-color: #8ceac9");
+   } else {
+      $('input[name=task12]').attr("style", "background-color: #e6476f");
+   }
+   if (notes.note1.time < hour) {
+      $('input[name=task1]').attr("style", "background-color: #c7c7c7");
+   } else if (notes.note1.time > hour) {
+      $('input[name=task1]').attr("style", "background-color: #8ceac9");
+   } else {
+      $('input[name=task1]').attr("style", "background-color: #e6476f");
+   }
+   if (notes.note2.time < hour) {
+      $('input[name=task2]').attr("style", "background-color: #c7c7c7");
+   } else if (notes.note2.time > hour) {
+      $('input[name=task2]').attr("style", "background-color: #8ceac9");
+   } else {
+      $('input[name=task2]').attr("style", "background-color: #e6476f");
+   }
+   if (notes.note3.time < hour) {
+      $('input[name=task3]').attr("style", "background-color: #c7c7c7");
+   } else if (notes.note3.time > hour) {
+      $('input[name=task3]').attr("style", "background-color: #8ceac9");
+   } else {
+      $('input[name=task3]').attr("style", "background-color: #e6476f");
+   }
+   if (notes.note4.time < hour) {
+      $('input[name=task4]').attr("style", "background-color: #c7c7c7");
+   } else if (notes.note4.time > hour) {
+      $('input[name=task4]').attr("style", "background-color: #8ceac9");
+   } else {
+      $('input[name=task4]').attr("style", "background-color: #e6476f");
+   }
+   if (notes.note5.time < hour) {
+      $('input[name=task5]').attr("style", "background-color: #c7c7c7");
+   } else if (notes.note5.time > hour) {
+      $('input[name=task5]').attr("style", "background-color: #8ceac9");
+   } else {
+      $('input[name=task5]').attr("style", "background-color: #e6476f");
+   }
+}
+setTasks();
+
+// change color of task depending on time of day
 
 function changeTask10() {
-   for (i = 0; notes.length; i++); {
-      // if ($('input[name=task10]').value === "") {
-      //    $('input[name=task10]').attr("style", "background-color: white");
-      // } else if ($('input[name=task8]').value !== "") {
-      //    $('input[name=task10]').attr("style", "background-color: orange");
-      // }
-      if (notes.note10.time < hour) {
-         $('input[name=task10]').attr("style", "background-color: orange");
-      } else if (notes.note10.time > hour) {
-         $('input[name=task10]').attr("style", "background-color: #d393f5");
-      } else {
-         $('input[name=task10]').attr("style", "background-color: #37a33b");
-      }
+   if (notes.note10.time < hour) {
+      $('input[name=task10]').attr("style", "background-color: #8669d6");
+   } else if (notes.note10.time > hour) {
+      $('input[name=task10]').attr("style", "background-color: #8ceac9");
+   } else {
+      $('input[name=task10]').attr("style", "background-color: #e6476f");
    }
 }
 
+
 function changeTask11() {
-   for (i = 0; notes.length; i++); {
-      if (notes.note11.time < hour) {
-         $('input[name=task11]').attr("style", "background-color: orange");
-      } else if (notes.note11.time > hour) {
-         $('input[name=task11]').attr("style", "background-color: #d393f5");
-      } else {
-         $('input[name=task11]').attr("style", "background-color: #37a33b");
-      }
+   if (notes.note11.time < hour) {
+      $('input[name=task11]').attr("style", "background-color: #8669d6");
+   } else if (notes.note11.time > hour) {
+      $('input[name=task11]').attr("style", "background-color: #8ceac9");
+   } else {
+      $('input[name=task11]').attr("style", "background-color: #e6476f");
    }
 }
 
 function changeTask12() {
-   for (i = 0; notes.length; i++); {
-      if (notes.note12.time < hour) {
-         $('input[name=task12]').attr("style", "background-color: orange");
-      } else if (notes.note12.time > hour) {
-         $('input[name=task12]').attr("style", "background-color: #d393f5");
-      } else {
-         $('input[name=task12]').attr("style", "background-color: #37a33b");
-      }
+   if (notes.note12.time < hour) {
+      $('input[name=task12]').attr("style", "background-color: #8669d6");
+   } else if (notes.note12.time > hour) {
+      $('input[name=task12]').attr("style", "background-color: #8ceac9");
+   } else {
+      $('input[name=task12]').attr("style", "background-color: #e6476f");
    }
 }
 
 function changeTask1() {
-   for (i = 0; notes.length; i++); {
-      if (notes.note1.time < hour) {
-         $('input[name=task1]').attr("style", "background-color: orange");
-      } else if (notes.note1.time > hour) {
-         $('input[name=task1]').attr("style", "background-color: #d393f5");
-      } else {
-         $('input[name=task1]').attr("style", "background-color: #37a33b");
-      }
+   if (notes.note1.time < hour) {
+      $('input[name=task1]').attr("style", "background-color: #8669d6");
+   } else if (notes.note1.time > hour) {
+      $('input[name=task1]').attr("style", "background-color: #8ceac9");
+   } else {
+      $('input[name=task1]').attr("style", "background-color: #e6476f");
    }
 }
 
 function changeTask2() {
-   for (i = 0; notes.length; i++); {
-      if (notes.note2.time < hour) {
-         $('input[name=task2]').attr("style", "background-color: orange");
-      } else if (notes.note2.time > hour) {
-         $('input[name=task2]').attr("style", "background-color: #d393f5");
-      } else {
-         $('input[name=task2]').attr("style", "background-color: #37a33b");
-      }
+   if (notes.note2.time < hour) {
+      $('input[name=task2]').attr("style", "background-color: #8669d6");
+   } else if (notes.note2.time > hour) {
+      $('input[name=task2]').attr("style", "background-color: #8ceac9");
+   } else {
+      $('input[name=task2]').attr("style", "background-color: #e6476f");
    }
 }
 
 function changeTask3() {
-   for (i = 0; notes.length; i++); {
-      if (notes.note3.time < hour) {
-         $('input[name=task3]').attr("style", "background-color: orange");
-      } else if (notes.note3.time > hour) {
-         $('input[name=task3]').attr("style", "background-color: #d393f5");
-      } else if (notes.note3.time = hour) {
-         $('input[name=task3]').attr("style", "background-color: #37a33b");
-      }
+   if (notes.note3.time < hour) {
+      $('input[name=task3]').attr("style", "background-color: #8669d6");
+   } else if (notes.note3.time > hour) {
+      $('input[name=task3]').attr("style", "background-color: #8ceac9");
+   } else if (notes.note3.time = hour) {
+      $('input[name=task3]').attr("style", "background-color: #e6476f");
    }
 }
 
 function changeTask4() {
-   for (i = 0; notes.length; i++); {
-      if (notes.note4.time < hour) {
-         $('input[name=task4]').attr("style", "background-color: orange");
-      } else if (notes.note4.time > hour) {
-         $('input[name=task4]').attr("style", "background-color: #d393f5");
-      } else if (notes.note4.time = hour) {
-         $('input[name=task4]').attr("style", "background-color: #37a33b");
-      }
+   if (notes.note4.time < hour) {
+      $('input[name=task4]').attr("style", "background-color: #8669d6");
+   } else if (notes.note4.time > hour) {
+      $('input[name=task4]').attr("style", "background-color: #8ceac9");
+   } else if (notes.note4.time = hour) {
+      $('input[name=task4]').attr("style", "background-color: #e6476f");
    }
 }
 
 function changeTask5() {
-   for (i = 0; notes.length; i++); {
-      if (notes.note5.time < hour) {
-         $('input[name=task5]').attr("style", "background-color: orange");
-      } else if (notes.note5.time > hour) {
-         $('input[name=task5]').attr("style", "background-color: #d393f5");
-      } else if (notes.note5.time = hour) {
-         $('input[name=task5]').attr("style", "background-color: #37a33b");
-      }
+   if (notes.note5.time < hour) {
+      $('input[name=task5]').attr("style", "background-color: #8669d6");
+   } else if (notes.note5.time > hour) {
+      $('input[name=task5]').attr("style", "background-color: #8ceac9");
+   } else if (notes.note5.time = hour) {
+      $('input[name=task5]').attr("style", "background-color: #e6476f");
    }
 }
 
